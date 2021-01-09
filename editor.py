3ebe1c0b3456c22c805cab1cc302cd994fe7c349
AdvendraDeswanta <adeswanta@gmail.com>
2021-01-09 15:09:50 +0700

    Editor script integration

--- editor.py
+++ editor.py
@@ -1,4 +1,4 @@
-import gi
+import gi, settings
 
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk, Gdk, Gio, GLib
@@ -13,10 +13,25 @@ def getPreviewStatus():
         editor.get_object("previewButton").set_sensitive(True)
     return GLib.SOURCE_CONTINUE
 
-class Handler:
+def setTitle(projectName, projectDir):
+    editor.get_object("editorTitle").set_title(projectName)
+    editor.get_object("editorTitle").set_subtitle(projectDir)
+
+class editorHandler:
+    def init(self, *args):
+        editor.get_object("aboutDialog").set_transient_for(editor.get_object("editorWindow"))
+        editor.get_object("settingsDialog").set_transient_for(editor.get_object("editorWindow"))
     def destroy(self, *args):
         previewProc.force_exit()
         Gtk.main_quit()
+    def settings(self, *args):
+        editor.get_object("settingsDialog").show_all()
+        settings.loadSetings(editor)
+    def settingsHide(self, *args):
+        editor.get_object("settingsDialog").hide()
+        return True
+    def about(self, *args):
+        editor.get_object("aboutDialog").show_all()
     def aboutHide(self, *args):
         editor.get_object("aboutDialog").hide()
         return True
@@ -24,10 +39,14 @@ class Handler:
         global previewProc
         previewProc = Gio.Subprocess.new(["python3", "preview.py"], 0)
         editor.get_object("previewButton").set_sensitive(False)
-    def about(self, *args):
-        editor.get_object("aboutDialog").show_all()
+    def settingAction(self, listbox, listboxrow):
+        if listboxrow.get_index() == 0:
+            settings.settingDir(editor.get_object("settingsDialog"), editor.get_object("settingDirLabel"))
+        elif listboxrow.get_index() == 1:
+            settings.settingEditor(editor.get_object("settingsDialog"), editor.get_object("settingEditorLabel"))
+        settings.saveSettings(editor)
 
-editor.connect_signals(Handler())
+editor.connect_signals(editorHandler())
 
 window = editor.get_object("editorWindow")
 window.show_all()
--- gtk-theme-creator.py
+++ gtk-theme-creator.py
@@ -1,27 +1,64 @@
-import gi
+import gi, settings
 
 gi.require_version("Gtk", "3.0")
-from gi.repository import Gtk, Gdk, Gio
+from gi.repository import Gtk, Gio
 
 welcome = Gtk.Builder()
 welcome.add_from_file("ui/welcome.ui")
 
-class Handler:
+editor = Gtk.Builder()
+editor.add_from_file("ui/editor.ui")
+
+def hide_window(window, event):
+    window.hide()
+    return True
+
+def settingAction(listbox, listboxrow):
+    if listboxrow.get_index() == 0:
+        settings.settingDir(editor.get_object("settingsDialog"), editor.get_object("settingDirLabel"))
+    elif listboxrow.get_index() == 1:
+        settings.settingEditor(editor.get_object("settingsDialog"), editor.get_object("settingEditorLabel"))
+    settings.saveSettings(editor)
+
+class welcomeHandler:
+    def init(self, *args):
+        editor.get_object("aboutDialog").set_transient_for(welcome.get_object("mainWindow"))
+        editor.get_object("aboutDialog").connect("delete-event", hide_window)
+        editor.get_object("settingsDialog").set_transient_for(welcome.get_object("mainWindow"))
+        editor.get_object("settingsDialog").connect("delete-event", hide_window)
+        editor.get_object("settingsList").connect("row-activated", settingAction)
     def destroy(self, *args):
         Gtk.main_quit()
-    def hideDialog(self, *args):
-        hideDialog()
     def newHide(self, *args):
         welcome.get_object("newProjectDialog").hide()
         return True
+    def settings(self, *args):
+        editor.get_object("settingsDialog").show_all()
+        settings.loadSetings(editor)
+    def about(self, *args):
+        editor.get_object("aboutDialog").show_all()
     def newProject(self, *args):
         welcome.get_object("newProjectDialog").show_all()
+        welcome.get_object("projectDir").set_text(settings.getSettings()["settingDir"])
     def openEditor(self, *args):
+        global projectName, projectDir
         welcome.get_object("newProjectDialog").hide()
         welcome.get_object("mainWindow").hide()
         import editor
+        editor.setTitle(welcome.get_object("projectName").get_text(), welcome.get_object("projectDir").get_text())
+    def browseDir(self, *args):
+        dialog = Gtk.FileChooserDialog(
+            title="Please choose a location", parent=welcome.get_object("newProjectDialog"), action=Gtk.FileChooserAction.SELECT_FOLDER
+        )
+        dialog.add_buttons(
+            Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL, "Select", Gtk.ResponseType.OK
+        )
+        response = dialog.run()
+        if response == Gtk.ResponseType.OK:
+            welcome.get_object("projectDir").set_text(dialog.get_filename()) 
+        dialog.destroy()
 
-welcome.connect_signals(Handler())
+welcome.connect_signals(welcomeHandler())
 
 mainWindow = welcome.get_object("mainWindow")
 mainWindow.show_all()
